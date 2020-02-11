98bfd2665d4c38fcea8f6f280a0dc027fe56bcea
gemary <ducdang1206@gmail.com>
2020-02-11 20:54:08 +0700

    new route

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -1,6 +1,12 @@
 const fs =require('fs')
 module.exports.index =async (req,res)=>{
-    const rawdata = fs.readFileSync('./data.json')
+    const rawdata = fs.readFileSync('../data/listPokemon.json')
+    const data= JSON.parse(rawdata)
+    res.json(data)  
+}
+
+module.exports.Forms =async(req,res)=>{
+    const rawdata = fs.readFileSync('../data/pokemonForm.json')
     const data= JSON.parse(rawdata)
     res.json(data)  
 }
--- api/routes/route.js
+++ api/routes/route.js
@@ -3,4 +3,5 @@ const router = app.Router()
 const controller =require('../controller/controller')
 
 router.get("/listPokemon",controller.index)
+router.get("/FormsPokemon",controller.index)
 module.exports = router;
